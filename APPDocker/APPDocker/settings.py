550ee0fbdfe0eedfe6edc7fa4f29979090a3c71c
MaxiVargas78 <maxx7689@gmail.com>
2021-12-27 21:02:40 -0300

    Storage S3 AWS

--- APPDocker/APPDocker/settings.py
+++ APPDocker/APPDocker/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'Juegos',
-
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -141,7 +141,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+AWS_ACCESS_KEY_ID = 'AKIAW5AGQ6EBEOYYC343'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'storage-arqui'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
